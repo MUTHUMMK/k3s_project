3f0fd22363be82d68ec34a7d886dc2921ef018bf
admin <admin@vidhvaa.com>
2026-03-08 15:39:52 +0530

    Modify main.tf

--- k3s_infra_create/main.tf
+++ k3s_infra_create/main.tf
@@ -21,12 +21,6 @@ resource "aws_security_group" "k3s_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-    ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   ingress {
     from_port   = 6443

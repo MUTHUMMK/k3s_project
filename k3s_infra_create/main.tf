48bc24d1eae833329270f922599e876412ba1c0e
admin <admin@vidhvaa.com>
2026-03-19 22:42:50 +0530

    main.tf updated

--- k3s_infra_create/main.tf
+++ k3s_infra_create/main.tf
@@ -75,28 +75,14 @@ resource "aws_instance" "k3s_server" {
               
               export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
               
-              # Install Helm
-              curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
               
-              helm version
-              
-              helm repo add argo https://argoproj.github.io/argo-helm
-              helm repo update
-              
-              kubectl create namespace argocd || true
-              
-              helm install argocd argo/argo-cd --namespace argocd --version 7.7.0 --wait
-              
-              sudo kubectl apply -f - <<< $'apiVersion: v1\nkind: Service\nmetadata:\n  name: argocd-server-nodeport\n  namespace: argocd\nspec:\n  type: NodePort\n  selector:\n    app.kubernetes.io/name: argocd-server\n  ports:\n  - name: http\n    port: 80\n    targetPort: 8080\n    nodePort: 30080\n  - name: https\n    port: 443\n    targetPort: 8080\n    nodePort: 30443'
-              
-              
-              # Deploy your container
-              sudo kubectl run nginx-5785977ddb-r9lmz --image=muthummkdh/muthu26:2c91a9b --port=80
-              
+              # Deploy your NGINX SERVER
+              sudo kubectl create deployment nginx --image=nginx:trixie-perl --port=80
+
               # Expose service
-              sudo kubectl expose pod nginx-5785977ddb-r9lmz --type=NodePort --port=80 --target-port=80
-              
-              sudo kubectl apply -f - <<< $'apiVersion: networking.k8s.io/v1\nkind: Ingress\nmetadata:\n  name: nginx-ingress\nspec:\n  rules:\n  - http:\n      paths:\n      - path: /\n        pathType: Prefix\n        backend:\n          service:\n            name: nginx-5785977ddb-r9lmz\n            port:\n              number: 80'
+              sudo kubectl expose deployment nginx --type=ClusterIP --port=80 --target-port=80 --name=nginx-service
+
+              sudo kubectl apply -f - <<< $'apiVersion: networking.k8s.io/v1\nkind: Ingress\nmetadata:\n  name: nginx-ingress\nspec:\n  rules:\n  - http:\n      paths:\n      - path: /\n        pathType: Prefix\n        backend:\n          service:\n            name: nginx-service\n            port:\n              number: 80'
               
               EOF
 
